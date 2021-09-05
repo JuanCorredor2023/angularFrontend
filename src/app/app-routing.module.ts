31d48c215bbb7e22d41d78b935b3d0f0f95f9108
Juan Andres Corredor <jcorredor2004@gmail.com>
2021-09-04 21:24:20 -0500

    [add] add all frontend folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { Error404Component } from './components/error404/error404.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewsComponent } from './components/news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:"home", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"news", component:NewsComponent},
+  {path:"login", component:LoginComponent},
+  {path:"**", component:Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NewsComponent } from './components/news/news.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavBarComponent } from './tools/nav-bar/nav-bar.component';
+import { FooterComponent } from './tools/footer/footer.component';
+import { Error404Component } from './components/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NewsComponent,
+    LoginComponent,
+    NavBarComponent,
+    FooterComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
